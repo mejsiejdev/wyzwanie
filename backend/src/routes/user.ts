fb075560d64ef233ab1a8b8354a0c08da1cc0ade
marcel-alefierowicz <mrclalefierowicz@gmail.com>
2023-03-17 16:03:25 +0100

    [WIP] Implemented adding users

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { prisma } from "../../lib/db";
+import bcrypt from "bcrypt";
+
 
 const router = express.Router();
 
@@ -8,5 +10,28 @@ router.get("/", async (req, res) => {
   const users = await prisma.user.findMany();
   res.json(users);
 });
+
+router.post('/', async(req,res) => {
+  const {name, password} = req.body;
+
+  bcrypt.hash(password, 3, async function(err, hash) {
+    if(err){
+      res.status(500)
+      console.error(err)
+    }
+    await prisma.user.create({
+      data: {
+        name: name,
+        password: hash
+      }
+    })
+  });
+
+
+
+
+
+})
+
 
 export default router;

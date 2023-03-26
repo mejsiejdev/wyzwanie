c65efe177b74d1aa40a80b293c5b85a63ae11917
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2023-03-26 14:43:00 +0200

    feat: :sparkles: `Dashboard` now fetches challenges

--- frontend/src/Routes/Dashboard.tsx
+++ frontend/src/Routes/Dashboard.tsx
@@ -1,25 +1,49 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { MdCheck } from "react-icons/md";
 import Box from "../components/Box";
+import { Button } from "react-bootstrap";
+
+type Challenge = {
+  id: string;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+  expiresAt: Date;
+  completedAt?: Date;
+  points: number;
+};
 
 const Dashboard = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [challenges, setChallenges] = useState<Challenge[]>();
+
+  const getChallenges = async () => {
+    setLoading(true);
+    await fetch("http://localhost:8000/challenge", {
+      method: "get",
+      mode: "cors",
+      headers: {
+        Authorization: `${sessionStorage.getItem("JWT")}`,
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((challanges) => setChallenges(() => challanges));
+    setLoading(false);
+  };
+
   useEffect(() => {
     // Check if JWT exists in the storage
     if (sessionStorage.getItem("JWT") === null) {
       navigate("/signin");
     }
     // Fetch challenges
-    fetch("http://localhost:8000/challenge", {
-      method: "get",
-      mode: "cors",
-      headers: {
-        Authorization: `${sessionStorage.getItem("JWT")}`,
-      },
-    }).then((response) => {
-      console.log(response.body);
-    });
+    getChallenges();
   }, []);
   return (
     <>
@@ -28,31 +52,26 @@ const Dashboard = () => {
         <span className="rounded-circle bg-info" style={{ height: "2rem", width: "2rem" }} />
       </div>
       <div className="d-flex flex-column gap-3 w-100">
-        {/* <Challange
-          content="Complete making the dashboard"
-          expiresAt={"2023-03-25T00:00:00Z"}
-          completedAt={""}
-        />
-        <Challange
-          content="Clean up the backend"
-          expiresAt={"2023-04-23T00:00:00Z"}
-          completedAt={""}
-        /> */}
-
+        {loading ? (
+          <p>Loading challenges...</p>
+        ) : challenges?.length !== 0 ? (
+          challenges?.map((challenge, key) => <Challenge key={key} {...challenge} />)
+        ) : (
+          <Box>
+            <div className="d-flex flex-row align-items-center justify-content-between gap-3">
+              <p className="mb-0">Looks like you don&apos;t have any challenges yet!</p>
+              <Link to={"/create"}>
+                <Button variant={"primary"}>Create challange</Button>
+              </Link>
+            </div>
+          </Box>
+        )}
       </div>
     </>
   );
 };
 
-const Challange = ({
-  content,
-  expiresAt,
-  completedAt,
-}: {
-  content: string;
-  expiresAt: string;
-  completedAt: string;
-}) => (
+const Challenge = ({ content, expiresAt, completedAt }: Challenge) => (
   <Box>
     <div className="d-flex flex-row align-items-center justify-content-between gap-3">
       <h3>{content}</h3>

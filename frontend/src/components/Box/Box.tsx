7d0479c639822b3e1796419ba4167aab0a8620df
Maciej Malinowski <mejsiejdev@gmail.com>
2023-04-12 12:22:57 +0200

    style: :art: Add border to `Box`

--- frontend/src/components/Box/Box.tsx
+++ frontend/src/components/Box/Box.tsx
@@ -1,4 +1,4 @@
 const Box = ({ children }: { children: React.ReactNode }) => (
-  <div className="shadow-sm p-3 rounded-3 bg-white w-100">{children}</div>
+  <div className="shadow-sm p-3 rounded-3 bg-white border border-light w-100">{children}</div>
 );
 export default Box;
